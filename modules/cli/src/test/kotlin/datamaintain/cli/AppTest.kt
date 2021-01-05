aed8d55d27190f56747813cbb82d17202b98337f
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-01-05 10:48:25 +0100

    refactor: Separate tests responsibilities

--- modules/cli/src/test/kotlin/datamaintain/cli/AppTest.kt
+++ modules/cli/src/test/kotlin/datamaintain/cli/AppTest.kt
@@ -3,11 +3,10 @@ package datamaintain.cli
 import com.github.ajalt.clikt.core.subcommands
 import datamaintain.core.config.DatamaintainConfig
 import datamaintain.core.step.check.rules.implementations.SameScriptsAsExecutedCheck
-import datamaintain.db.driver.mongo.MongoDriverConfig
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.*
-import java.nio.file.Paths
 
 internal class AppTest {
     data class ConfigWrapper(var datamaintainConfig: DatamaintainConfig? = null)
@@ -18,62 +17,48 @@ internal class AppTest {
         configWrapper.datamaintainConfig = config
     }
 
-    @Test
-    fun `should build config with one rule`() {
-        // Given
-        val argv = getSmallestArgs().plus(listOf(
-                "--rule", SameScriptsAsExecutedCheck.NAME
-        ))
+    @Nested
+    inner class Rules {
+        @Test
+        fun `should build config with one rule`() {
+            // Given
+            val argv = getSmallestArgs().plus(listOf(
+                    "--rule", SameScriptsAsExecutedCheck.NAME
+            ))
 
-        // When
-        App().subcommands(UpdateDb(runner = ::runner), ListExecutedScripts()).main(argv)
+            // When
+            App().subcommands(UpdateDb(runner = ::runner), ListExecutedScripts()).main(argv)
 
-        // Then
-        defaultChecks(configWrapper)
-        expectThat(configWrapper) {
-            get { datamaintainConfig }.isNotNull()
+            // Then
+            expectThat(configWrapper) {
+                get { datamaintainConfig }.isNotNull()
+            }
+            expectThat(configWrapper.datamaintainConfig!!.checkRules.toList()) {
+                hasSize(1)
+                first().isEqualTo(SameScriptsAsExecutedCheck.NAME)
+            }
         }
-        expectThat(configWrapper.datamaintainConfig!!.checkRules.toList()) {
-            hasSize(1)
-            first().isEqualTo(SameScriptsAsExecutedCheck.NAME)
-        }
-    }
 
-    @Test
-    fun `should build config with 2 rules`() {
-        // Given
-        val argv = getSmallestArgs().plus(listOf(
-                "--rule", SameScriptsAsExecutedCheck.NAME,
-                "--rule", SameScriptsAsExecutedCheck.NAME
-        ))
+        @Test
+        fun `should build config with 2 rules`() {
+            // Given
+            val argv = getSmallestArgs().plus(listOf(
+                    "--rule", SameScriptsAsExecutedCheck.NAME,
+                    "--rule", SameScriptsAsExecutedCheck.NAME
+            ))
 
-        // When
-        App().subcommands(UpdateDb(runner = ::runner), ListExecutedScripts()).main(argv)
+            // When
+            App().subcommands(UpdateDb(runner = ::runner), ListExecutedScripts()).main(argv)
 
-        // Then
-        defaultChecks(configWrapper)
-        expectThat(configWrapper) {
-            get { datamaintainConfig }.isNotNull()
-        }
-        expectThat(configWrapper.datamaintainConfig!!.checkRules.toList()) {
-            hasSize(2)
-            first().isEqualTo(SameScriptsAsExecutedCheck.NAME)
-            last().isEqualTo(SameScriptsAsExecutedCheck.NAME)
-        }
-    }
-
-    private fun defaultChecks(configWrapper: ConfigWrapper) {
-        expectThat(configWrapper) {
-            get { datamaintainConfig }
-                    .isNotNull()
-                    .get { driverConfig }
-                    .isA<MongoDriverConfig>()
-                    .get { mongoUri }
-                    .isEqualTo("localhost:27017")
-            get { datamaintainConfig }
-                    .isNotNull()
-                    .get { path }
-                    .isEqualTo(Paths.get("/tmp"))
+            // Then
+            expectThat(configWrapper) {
+                get { datamaintainConfig }.isNotNull()
+            }
+            expectThat(configWrapper.datamaintainConfig!!.checkRules.toList()) {
+                hasSize(2)
+                first().isEqualTo(SameScriptsAsExecutedCheck.NAME)
+                last().isEqualTo(SameScriptsAsExecutedCheck.NAME)
+            }
         }
     }
 
